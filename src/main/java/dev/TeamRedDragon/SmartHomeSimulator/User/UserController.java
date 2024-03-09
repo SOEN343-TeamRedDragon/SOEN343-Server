6898782ab202fa13ee7a0f33623eb655faf2cf76
NicolasChelico <Nicolas.chelico@outlook.com>
2024-03-09 12:33:37 -0500

    Added cors to enable front end and backend configs

--- src/main/java/dev/TeamRedDragon/SmartHomeSimulator/User/UserController.java
+++ src/main/java/dev/TeamRedDragon/SmartHomeSimulator/User/UserController.java
@@ -17,7 +17,7 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @GetMapping("/")
+    @GetMapping
     public List<User> getUsers(){
         return userService.getUsers();
          

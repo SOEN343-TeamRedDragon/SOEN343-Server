604130aa128ef472454b8aadbab3d82ddcb031cd
Houssam Righi <hrighi01@gmail.com>
2024-03-09 11:02:30 -0500

    Add getUsers() method to UserController and UserService

--- src/main/java/dev/TeamRedDragon/SmartHomeSimulator/User/UserController.java
+++ src/main/java/dev/TeamRedDragon/SmartHomeSimulator/User/UserController.java
@@ -1,7 +1,10 @@
 package dev.TeamRedDragon.SmartHomeSimulator.User;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +16,12 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @GetMapping("/")
+    public List<User> getUsers(){
+        return userService.getUsers();
+         
+    }
 
     @PostMapping("/AddUser")
     public String addUser(@RequestBody User user){

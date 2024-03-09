c07067b2fb8bebb1d2695478e929b84c997b377f
NicolasChelico <Nicolas.chelico@outlook.com>
2024-03-09 10:45:24 -0500

    Created the User model with basic put request

--- src/main/java/dev/TeamRedDragon/SmartHomeSimulator/User/User.java
+++ src/main/java/dev/TeamRedDragon/SmartHomeSimulator/User/User.java
@@ -1,4 +1,81 @@
 package dev.TeamRedDragon.SmartHomeSimulator.User;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.EnumMap;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+    private String userName;
+    private String password;
+    private String role;
+
+    public User() {
+    }
+
+    public User(Integer id, String name, String userName, String password, String role) {
+        this.id = id;
+        this.name = name;
+        this.userName = userName;
+        this.password = password;
+        this.role = role;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
 }
--- src/main/java/dev/TeamRedDragon/SmartHomeSimulator/User/UserController.java
+++ src/main/java/dev/TeamRedDragon/SmartHomeSimulator/User/UserController.java
@@ -1,4 +1,23 @@
 package dev.TeamRedDragon.SmartHomeSimulator.User;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/User")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/AddUser")
+    public String addUser(@RequestBody User user){
+        userService.saveUser(user);
+        return "User is sucessfully added!";
+    }
+
 }

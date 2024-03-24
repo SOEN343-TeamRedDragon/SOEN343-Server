8b237460500a6bf20474a65ead77e89fade74f4a
ceyhuntopcu <ceyhuntopcu4107@gmail.com>
2024-03-24 13:59:20 -0400

    Add, remove, and update zone functionality

--- src/main/java/dev/TeamRedDragon/SmartHomeSimulator/Zone/ZoneController.java
+++ src/main/java/dev/TeamRedDragon/SmartHomeSimulator/Zone/ZoneController.java
@@ -3,6 +3,7 @@ package dev.TeamRedDragon.SmartHomeSimulator.Zone;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List; 
 import java.util.Map;
 
 @RestController
@@ -11,7 +12,7 @@ public class ZoneController {
 
     @Autowired
     private ZoneService zoneService;
-
+ 
     @PostMapping("/CreateZone")
     public Zone createZone(@RequestBody Map<String, Double> data) {
         double zoneId = data.get("zoneId");
@@ -23,8 +24,28 @@ public class ZoneController {
 
     @PostMapping("/AddRoomToZone") 
     public Zone addRoomToZoneByZoneIdAndRoomId(@RequestBody Map<String, Integer> data) {
-        int zoneId = data.get("zoneId");
+        int zoneId = data.get("zoneId"); 
         int roomId = data.get("roomId");
         return zoneService.addRoomToZoneByZoneIdAndRoomId(zoneId, roomId);
     }
+
+    @PostMapping("/RemoveRoomFromZone")
+    public Zone removeRoomFromZoneByZoneIdAndRoomId(@RequestBody Map<String, Integer> data) {
+        int zoneId = data.get("zoneId");
+        int roomId = data.get("roomId");
+        return zoneService.removeRoomFromZoneByZoneIdAndRoomId(zoneId, roomId);
+    }
+
+    @GetMapping("/GetZones")
+    public List<String> getZones() {
+        return zoneService.getZonesDetailedInfo();
+    }
+
+    @PutMapping("/UpdateZoneTemperature")
+    public Zone updateZoneTemperature(@RequestBody Map<String, Object> data) {
+        int zoneId = (int) data.get("zoneId"); 
+        String timeOfDay = (String) data.get("timeOfDay");
+        double newTemperature = ((Number) data.get("newTemperature")).doubleValue();
+        return zoneService.updateZoneTemperature(zoneId, timeOfDay, newTemperature);
+}
 }

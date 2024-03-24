0cbb8e3350861d3d9afda0c4c61d4b7f3aca2b49
DanDuguay <Duguay9@gmail.com>
2024-03-24 18:27:35 -0400

    it works.

--- src/main/java/dev/TeamRedDragon/SmartHomeSimulator/Modules/SmartHeatingModule/SmartHeatingModule.java
+++ src/main/java/dev/TeamRedDragon/SmartHomeSimulator/Modules/SmartHeatingModule/SmartHeatingModule.java
@@ -1,22 +1,31 @@
 package dev.TeamRedDragon.SmartHomeSimulator.Modules.SmartHeatingModule;
 
+import dev.TeamRedDragon.SmartHomeSimulator.Command.OffCommand;
+import dev.TeamRedDragon.SmartHomeSimulator.Home.Home;
 import dev.TeamRedDragon.SmartHomeSimulator.Observer.Observer;
 import dev.TeamRedDragon.SmartHomeSimulator.SimulationClock.SimulationClock;
+import dev.TeamRedDragon.SmartHomeSimulator.SmartElement.AirConditioner;
 import dev.TeamRedDragon.SmartHomeSimulator.SmartElement.Heater;
 import dev.TeamRedDragon.SmartHomeSimulator.SmartElement.SmartElement;
 
 import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Objects;
 
 import dev.TeamRedDragon.SmartHomeSimulator.Command.Command;
 import dev.TeamRedDragon.SmartHomeSimulator.Room.Room;
 import dev.TeamRedDragon.SmartHomeSimulator.Command.SetTemperatureCommand;
+import dev.TeamRedDragon.SmartHomeSimulator.TemperatureData.TemperatureDataService;
 
 public class SmartHeatingModule implements Observer {
+
+    Command command;
 
     private static SmartHeatingModule smartHeatingModule;
 
     private static SimulationClock simulationClock = SimulationClock.getSimulationClock();
+
+    private Home home = Home.getHome();
 
     private ArrayList<Room> coolZoneList = new ArrayList<Room>();
     private ArrayList<Room> heatZoneList = new ArrayList<Room>();
@@ -56,8 +65,25 @@ public class SmartHeatingModule implements Observer {
         return smartHeatingModule;
     }
 
+    public void updateRoomTempByOutdoorTemp() {
+        for (Room room : home.getRoomList())
+        {
+            if(TemperatureDataService.IsCoolerOutside(room)){
+                for (SmartElement element : room.getSmartElementList())
+                {
+                    if (Objects.equals(element.getElementType(), "AirConditioner"))
+                    {
+                        command = new OffCommand(element);
+                        element.setCommand(command);
+                        element.executeCommand();
+                    }
+                }
+            }
+        }
+    }
+
     @Override
     public void update() {
-
+        updateRoomTempByOutdoorTemp();
     }
 }

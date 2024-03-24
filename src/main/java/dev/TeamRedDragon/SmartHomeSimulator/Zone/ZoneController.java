7921a10b5f542f96ad0861735eb194f385c6a2a2
ceyhuntopcu <ceyhuntopcu4107@gmail.com>
2024-03-24 13:05:06 -0400

    Refactor ZoneController.java to use @PostMapping and update method signature

--- src/main/java/dev/TeamRedDragon/SmartHomeSimulator/Zone/ZoneController.java
+++ src/main/java/dev/TeamRedDragon/SmartHomeSimulator/Zone/ZoneController.java
@@ -1,12 +1,8 @@
 package dev.TeamRedDragon.SmartHomeSimulator.Zone;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -18,17 +14,17 @@ public class ZoneController {
 
     @PostMapping("/CreateZone")
     public Zone createZone(@RequestBody Map<String, Double> data) {
-        double zoneId =  data.get("zoneId");
+        double zoneId = data.get("zoneId");
         double amTemp = data.get("amTemp");
         double pmTemp = data.get("pmTemp");
         double nightTemp = data.get("nightTemp");
         return zoneService.createZone(zoneId, amTemp, pmTemp, nightTemp);
     }
 
-    @PostMapping("/AddRoomToZone")
-    public Zone addRoomTZoneByZoneIdAndRoomId(@RequestBody Map<String, String> data) {
-        String zoneId = data.get("zoneId");
-        String roomId = data.get("roomId");
-        return zoneService.addRoomToZoneByZoneIdAndRoomId(Integer.parseInt(zoneId), Integer.parseInt(roomId));
+    @PostMapping("/AddRoomToZone") 
+    public Zone addRoomToZoneByZoneIdAndRoomId(@RequestBody Map<String, Integer> data) {
+        int zoneId = data.get("zoneId");
+        int roomId = data.get("roomId");
+        return zoneService.addRoomToZoneByZoneIdAndRoomId(zoneId, roomId);
     }
 }
